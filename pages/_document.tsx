410e7ea07ab7b305f28f10df3cd0fa9dfbca296b
rwietter <mauriciobw17@gmail.com>
2022-11-15 15:17:11 -0300

    feat: improvement SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,15 @@ export default class extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5JBCGS5');`,
+            }}
+          />
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
@@ -59,16 +68,13 @@ export default class extends Document {
           <meta name="msapplication-square310x310logo" content="/icons/mstile-310x310.png" />
         </Head>
         <body>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5JBCGS5"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JBCGS5" height="0" width="0" style="display: none; visibility: hidden;" />',
+            }}
+          />
         </body>
       </Html>
     );
