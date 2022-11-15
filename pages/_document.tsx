3043c739886a493a4e0818afb21489f3787ef6b0
rwietter <mauriciobw17@gmail.com>
2022-11-15 14:21:11 -0300

    feat: improvement SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ import Document, {
 import { getCssText } from '../styles/theme';
 
 const APP_NAME = 'rwietter';
-const APP_DESCRIPTION = 'Software developer, passionate about technology, design and software engineering';
 
 export default class extends Document {
   static async getInitialProps(ctx: any) {
@@ -30,7 +29,6 @@ export default class extends Document {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-          <meta name="description" content={APP_DESCRIPTION} />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#FFFFFF" />
