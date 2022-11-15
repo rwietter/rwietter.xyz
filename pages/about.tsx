dd4b860ee89b1db33108e430315a38a8b432fdb6
rwietter <mauriciobw17@gmail.com>
2022-11-15 00:57:38 -0300

    fix: title

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,16 @@
-import Head from 'next/head';
+import SEO from 'components/SEO';
 import Link from 'next/link';
 import React from 'react';
 
 const About = () => (
   <>
-    <Head>
-      <title>Mauricio W.</title>
-    </Head>
+    <SEO
+      title="Sobre | Mauricio W. | Software Developer"
+      description="Software developer, passionate about technology, design and software engineering"
+      image="https://res.cloudinary.com/ddwnioveu/image/upload/v1651191166/profile/wallhaven-dpo7wm_1366x768_mdztjw.png"
+      content="website"
+      url="https://rwietter.xyz/"
+    />
     <h1>About</h1>
     <Link href="/" rel="canonical">Home</Link>
   </>
