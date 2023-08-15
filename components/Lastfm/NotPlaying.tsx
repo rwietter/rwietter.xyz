19f640b4a577a6faf223c6da636c2a0e5a7420bf
rwietter <rwietter@zohomail.com>
2023-08-15 13:14:52 -0300

    update border and remove image container

--- components/Lastfm/NotPlaying.tsx
+++ components/Lastfm/NotPlaying.tsx
@@ -2,14 +2,12 @@ import { FC } from 'react';
 import { BsSpotify } from 'react-icons/bs';
 import { MdMusicOff } from 'react-icons/md';
 import {
-  Playing, PlayingImage, PlayingArtist, PlayingSpot,
+  Playing, PlayingArtist, PlayingSpot,
 } from './styles';
 
 export const NotPlaying: FC = () => (
   <Playing>
-    <PlayingImage>
-      <MdMusicOff size={90} color="var(--colors-sub_color)" />
-    </PlayingImage>
+    <MdMusicOff size={90} color="var(--colors-sub_color)" />
     <PlayingArtist>
       <PlayingSpot>
         <BsSpotify size={19} color="#1DB954" />
