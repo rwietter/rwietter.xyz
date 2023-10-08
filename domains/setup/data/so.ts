9bde16fbb1d3ac58f3c6c91d9808780d639969d4
rwietter <rwietter@zohomail.com>
2023-10-08 11:47:02 -0300

    docs: update setup info

--- domains/setup/data/so.ts
+++ domains/setup/data/so.ts
@@ -3,7 +3,7 @@ import { SetupData } from '../types/setup-data';
 export const operationSystem: SetupData[] = [
   {
     name: 'Operational System',
-    url: 'https://archlinux.org/download/',
+    url: 'https://archlinux.org/',
     type: 'Arch Linux',
   },
   {
@@ -48,7 +48,7 @@ export const operationSystem: SetupData[] = [
   },
   {
     name: 'Headset',
-    url: '#',
+    url: 'https://www.amazon.com.br/compat%C3%ADvel-K1-cancelamento-microfone-Camouflage/dp/B087C4K92K/ref=sr_1_14?keywords=onikuma+k1b&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d',
     type: 'Onikuma K1-B',
   },
   {
@@ -58,7 +58,7 @@ export const operationSystem: SetupData[] = [
   },
   {
     name: 'Cadeira',
-    url: '#',
+    url: 'https://www.comfy.com.br/cadeira-de-escritorio-comfy-way-tela-mesh-preta-base-giratoria-e-sistema-relax.html',
     type: 'Comfy Way Tela Mesh Preta, Base Giratória e Sistema Relax',
   },
 ];
