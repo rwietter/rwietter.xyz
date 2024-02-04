997afea0f43f5e8ce32e6dac8dadc47bd2c61558
rwietter <rwietter@zohomail.com>
2024-02-03 23:24:06 -0300

    feat: add i18n for datetime component

--- utils/get-date.ts
+++ utils/get-date.ts
@@ -1,9 +1,26 @@
 import { format } from 'date-fns'
+import { enUS, ptBR } from 'date-fns/locale'
 
-export const getDate = (date: Date): string | null => {
-  const formatDate = `${format(date, "dd'rd' MMMM yyyy")} • ${format(
-    date,
-    'HH:mm:ss a',
-  )}`
-  return formatDate || null
+const supportedLocales = {
+  en: enUS,
+  pt: ptBR,
+}
+
+export type LocaleLang = 'en' | 'pt'
+
+export const getDate = (date: Date, locale: LocaleLang): string | null => {
+  const lang = supportedLocales[locale] || supportedLocales.pt
+
+  const localeIndexedLoockup = {
+    en: () =>
+      `${format(date, "dd'rd' MMMM yyyy", {
+        locale: lang,
+      })} • ${format(date, 'HH:mm:ss a', { locale: lang })}`,
+    pt: () =>
+      `${format(date, 'dd MMMM yyyy', {
+        locale: lang,
+      })} • ${format(date, 'HH:mm:ss a', { locale: lang })}`,
+  }
+
+  return localeIndexedLoockup[locale] ? localeIndexedLoockup[locale]() : null
 }
