c041fa54591994837d191a24ff9efb61c2543ac5
rwietter <rwietter@zohomail.com>
2024-02-10 12:32:23 -0300

    fix no playing

--- src/components/Lastfm/NotPlaying.tsx
+++ src/components/Lastfm/NotPlaying.tsx
@@ -1,16 +1,16 @@
 import { FC } from 'react'
 import { BsSpotify } from 'react-icons/bs'
 import { MdMusicOff } from 'react-icons/md'
-import { Playing, PlayingArtist, PlayingSpot } from './styles'
+import styles from './styles.module.css'
 
 export const NotPlaying: FC = () => (
-  <Playing>
+  <div className={styles.playing}>
     <MdMusicOff size={90} color='var(--colors-sub_color)' />
-    <PlayingArtist>
-      <PlayingSpot>
+    <div className={styles.playingArtist}>
+      <div className={styles.playingSpot}>
         <BsSpotify size={19} color='#1DB954' />
         <span className='artist'>Not listening to anything</span>
-      </PlayingSpot>
-    </PlayingArtist>
-  </Playing>
+      </div>
+    </div>
+  </div>
 )
