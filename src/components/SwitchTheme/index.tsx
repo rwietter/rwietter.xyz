ff76135132468a2a24e2d5aee8cf4fecd5236f02
rwietter <rwietter@zohomail.com>
2024-02-12 17:06:43 -0300

    feat: add store and load for theme

--- src/components/SwitchTheme/index.tsx
+++ src/components/SwitchTheme/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC, useRef } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import { MdSunny } from 'react-icons/md'
 import styles from './styles.module.css'
 
@@ -8,12 +8,29 @@ interface Props {
   visible: 'header' | 'sticky'
 }
 
+const updateTheme = () => {
+  const theme = localStorage.getItem('theme') || ''
+  const html = document.querySelector('html')
+  if (!html) return
+  html.classList.value = theme
+}
+
+const storeTheme = () => {
+  const theme = document.querySelector('html')?.classList?.value || ''
+  localStorage.setItem('theme', theme)
+}
+
 const SwitchTheme: FC<Props> = ({ visible }) => {
   const buttonRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    updateTheme()
+  }, [])
 
   const handleSwitchTheme = () => {
     buttonRef.current?.setAttribute('data-active', 'true')
     document.querySelector('html')?.classList.toggle('dark')
+    storeTheme()
 
     const timeout = setTimeout(() => {
       buttonRef.current?.removeAttribute('data-active')
